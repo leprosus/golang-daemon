5ac4324235e6f4bb4c0bb07cf6b6a82ad1a3bb05
Denis Korolev <admin@leprosus.ru>
2017-07-05 13:32:12 +0200

    Add: Add user access control

--- daemon.go
+++ daemon.go
@@ -5,14 +5,32 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+	"strings"
 )
 
 type Daemon struct {
-	withCli bool
+	withCli    bool
+	allowUsers []string
 }
+
+const (
+	help = "Usage:\n" +
+		"\trun\tto run script in foreground mode\n" +
+		"\tstart\tto start as daemon\n" +
+		"\tstop\tto stop daemon\n" +
+		"\trestart\tto restart of the daemon\n" +
+		"\tstatus\treturns daemon status\n" +
+		"\thelp\tto print this help\n"
+
+	havntAccess = "You havn't access to execute"
+)
 
 func New() Daemon {
 	return Daemon{}
+}
+
+func (daemon *Daemon) AllowUser(userName string) {
+	daemon.allowUsers = append(daemon.allowUsers, userName)
 }
 
 func (daemon *Daemon) StartWithCLI(mainLoop func()) (err error) {
@@ -21,14 +39,30 @@ func (daemon *Daemon) StartWithCLI(mainLoop func()) (err error) {
 
 		switch os.Args[1] {
 		case "run":
-			mainLoop()
+			if daemon.IsAllowExec() {
+				mainLoop()
+			} else {
+				println(havntAccess)
+			}
 		case "start":
-			err = daemon.Start(mainLoop)
+			if daemon.IsAllowExec() {
+				err = daemon.Start(mainLoop)
+			} else {
+				println(havntAccess)
+			}
 		case "restart":
-			daemon.Stop()
-			err = daemon.Start(mainLoop)
+			if daemon.IsAllowExec() {
+				daemon.Stop()
+				err = daemon.Start(mainLoop)
+			} else {
+				println(havntAccess)
+			}
 		case "stop":
-			err = daemon.Stop()
+			if daemon.IsAllowExec() {
+				err = daemon.Stop()
+			} else {
+				println(havntAccess)
+			}
 		case "status":
 			if daemon.IsDaemonised() {
 				println("run")
@@ -36,14 +70,6 @@ func (daemon *Daemon) StartWithCLI(mainLoop func()) (err error) {
 				println("stop")
 			}
 		default:
-			help := "Usage:\n" +
-				"\trun\tto run script in foreground mode\n" +
-				"\tstart\tto start as daemon\n" +
-				"\tstop\tto stop daemon\n" +
-				"\trestart\tto restart of the daemon\n" +
-				"\tstatus\treturns daemon status\n" +
-				"\thelp\tto print this help\n"
-
 			println(help)
 		}
 	} else {
@@ -97,6 +123,27 @@ func (daemon Daemon) IsDaemonised() bool {
 		var out []byte
 		out, _ = exec.Command("pgrep", "-f", path).Output()
 		return len(out) > 0
+	}
+
+	return false
+}
+
+func (daemon Daemon) IsAllowExec() bool {
+	if len(daemon.allowUsers) == 0 {
+		return true
+	}
+
+	bytes, err := exec.Command("whoami", ).Output()
+	if err != nil {
+		return false
+	}
+
+	curUser := strings.TrimSpace(string(bytes))
+
+	for _, user := range daemon.allowUsers {
+		if curUser == user {
+			return true
+		}
 	}
 
 	return false
